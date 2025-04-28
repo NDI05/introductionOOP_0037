a6297286711ed1a5bd69450febc7d0b31a784a69
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-04-28 10:21:00 +0700

    feat: create void on public class

--- class.cpp
+++ class.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 using namespace std;
 
-class Mobil{
-    public:
-        string merkMobil, tipeMobil;
-    private:
-        int hargaMobil;
+class Mobil
+{
+public:
+    string merkMobil, tipeMobil;
+
+private:
+    int hargaMobil;
+
+public:
+    void inputData(){
+        cout << "Masukkan Nama Mobil Anda: ";
+        cin >> merkMobil;
+        cout << "Masukkan tipeMobil Anda: ";
+        cin >> tipeMobil;
+    }
 };
 
-int main(){
+int main()
+{
     Mobil kt;
 }
